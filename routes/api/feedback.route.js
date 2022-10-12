106c53a920d6bfe437f76a7f011c20b299569cc3
PedroBlanco23 <pedriblanco@gmail.com>
2022-10-12 01:29:19 -0300

    Se agrego el controller y service de feedback

--- routes/api/feedback.route.js
+++ routes/api/feedback.route.js
@@ -6,7 +6,10 @@ var Authorization = require('../../auth/authorization');
 
 // Authorize each API with middleware and map to the Controller Functions
 /* GET users listing. */
-router.get('/', FeedbackController.getFeedback);
+router.get('/:_id', FeedbackController.getFeedback);
+router.get('/getLastFeedbackByRoutine/:_id', FeedbackController.getLastFeedbackByRoutine);
+router.put('/:_id', FeedbackController.putFeedbackById);
+
 
 // Export the Router
 module.exports = router;
